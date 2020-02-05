2262ae27a1f6c7b782f135085a7a2a4ecef36623
Takahashi_Yoshitomo <takahashi_yoshitomo@gitechnos.com>
2020-02-05 17:45:17 +0900

    機能実装、途中経過

--- app/src/main/java/com/example/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/calculator/CalculatorActivity.kt
@@ -1,8 +1,10 @@
 package com.example.calculator
 
 import android.content.pm.ActivityInfo
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_calculator.*
 
 class CalculatorActivity : AppCompatActivity() {
 
@@ -11,5 +13,52 @@ class CalculatorActivity : AppCompatActivity() {
         setContentView(R.layout.activity_calculator)
         //縦画面で固定
         ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        val calculateAreaText = calculate_area
+        val outputAreaText = output_area
+        val button00 = number_0_button
+        val button01 = number_1_button
+        val button02 = number_2_button
+        val button03 = number_3_button
+        val button04 = number_4_button
+        val button05 = number_5_button
+        val button06 = number_6_button
+        val button07 = number_7_button
+        val button08 = number_8_button
+        val button09 = number_9_button
+        calculateAreaText.setBackgroundColor(Color.WHITE)
+        outputAreaText.setBackgroundColor(Color.WHITE)
+
+        button00.setOnClickListener {
+            calculateAreaText.text = "0"
+        }
+        button01.setOnClickListener {
+
+        }
+        button02.setOnClickListener {
+
+        }
+        button03.setOnClickListener {
+
+        }
+        button04.setOnClickListener {
+
+        }
+        button05.setOnClickListener {
+
+        }
+        button06.setOnClickListener {
+
+        }
+        button07.setOnClickListener {
+
+        }
+        button08.setOnClickListener {
+
+        }
+        button09.setOnClickListener {
+
+        }
+
     }
 }
